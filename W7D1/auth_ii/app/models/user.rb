dc1ae527d884f4279c76f39a8438c17214ec725f
Zaid Mansuri <gbzaid@gmail.com>
2019-11-03 21:10:49 -0500

    added method headers to User model

--- W7D1/auth_ii/app/models/user.rb
+++ W7D1/auth_ii/app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ApplicationRecord
+
+    validates :username, presence: true
+    validates :password_digest, presence: { message: 'Password cannot be blank'}
+    validates :session_token, presense: true
+
+    def self.find_by_credentials(username, password)
+        
+    end
+
+    def generate_session_token
+    end
+
+
+    def reset_session_token!
+    end
+
+    def ensure_session_token
+    end
+
+    def password=
+    end
+
+
+
 end
